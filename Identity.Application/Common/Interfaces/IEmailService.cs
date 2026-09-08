fc39ca2b2a3ce31d3b8c587bd4b88f937847d985
Donia Shaban <doniashaban723@gmail.com>
2026-09-08 05:11:14 +0300

    feat(application): add IEmailService abstraction for email delivery

---
+++ Identity.Application/Common/Interfaces/IEmailService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Application.Common.Interfaces
+{
+    public interface IEmailService
+    {
+        Task<bool> SendConfirmationCodeAsync(
+            string toEmail,
+            string userName,
+            string confirmationCode,
+            CancellationToken cancellationToken = default);
+    }
+}
