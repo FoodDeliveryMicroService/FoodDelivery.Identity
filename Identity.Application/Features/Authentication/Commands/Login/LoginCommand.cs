b5934e18e5ce5cab5d452ca55d1dbe2493b4a35f
Donia Shaban <doniashaban723@gmail.com>
2026-09-10 15:19:47 +0300

    feat(application): add login command, handler, and validator

---
+++ Identity.Application/Features/Authentication/Commands/Login/LoginCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Identity.Application.Features.Authentication.Dtos.Login;
+using Identity.Domain.Common.Results;
+using MediatR;
+
+namespace Identity.Application.Features.Authentication.Commands.Login;
+
+public sealed record LoginCommand(LoginRequest Request)
+    : IRequest<Result<LoginResponse>>;
---
+++ Identity.Application/Features/Authentication/Commands/Login/LoginCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Identity.Application.Features.Authentication.Commands.Login;
+
+public sealed class LoginCommandValidator : AbstractValidator<LoginCommand>
+{
+    public LoginCommandValidator()
+    {
+        RuleFor(x => x.Request.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.Request.Password)
+            .NotEmpty();
+    }
+}
