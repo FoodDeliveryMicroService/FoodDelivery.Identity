407fe6550886726273400ea3de6d51e572748390
Donia Shaban <doniashaban723@gmail.com>
2026-09-10 15:19:43 +0300

    feat(application): add login, refresh token, and logout DTOs

---
+++ Identity.Application/Features/Authentication/Dtos/Logout/LogoutRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Application.Features.Authentication.Dtos.Logout;
+
+public sealed record LogoutRequest(
+    string RefreshToken
+);
---
+++ Identity.Application/Features/Authentication/Dtos/RefreshToken/RefreshTokenRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Application.Features.Authentication.Dtos.RefreshToken;
+
+public sealed record RefreshTokenRequest(
+    string RefreshToken
+);
