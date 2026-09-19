e7a85b4001b3a00e9d6aef8e5f9701186cd1286a
Donia Shaban <doniashaban723@gmail.com>
2026-09-19 15:22:34 +0300

    feat(address): add internal ResolveAddressById query for service-to-service checkout resolution (FR-17)

---
+++ Identity.Application/Features/AddressManagement/Queries/ResolveAddressById/ResolveAddressByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using Identity.Application.Features.AddressManagement.Dtos;
+using Identity.Application.Features.AddressManagement.Dtos.AddAddress;
+using Identity.Domain.Common.Results;
+using MediatR;
+
+namespace Identity.Application.Features.AddressManagement.Queries.ResolveAddressById;
+
+public sealed record ResolveAddressByIdQuery(Guid AddressId, Guid CustomerId) : IRequest<Result<AddressDto>>;
