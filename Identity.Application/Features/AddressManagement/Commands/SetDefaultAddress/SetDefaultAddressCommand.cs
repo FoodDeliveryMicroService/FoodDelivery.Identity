b49606720d83212294cb6cd50686505b7aa10cbc
Donia Shaban <doniashaban723@gmail.com>
2026-09-19 15:22:33 +0300

    feat(address): add SetDefaultAddress command handler and validator (FR-16)

---
+++ Identity.Application/Features/AddressManagement/Commands/SetDefaultAddress/SetDefaultAddressCommand.cs
@@ -0,0 +1,7 @@
+﻿using Identity.Domain.Common.Results;
+using MediatR;
+
+namespace Identity.Application.Features.AddressManagement.Commands.SetDefaultAddress;
+
+public sealed record SetDefaultAddressCommand(Guid AddressId, Guid CustomerId)
+    : IRequest<Result<Updated>>;
