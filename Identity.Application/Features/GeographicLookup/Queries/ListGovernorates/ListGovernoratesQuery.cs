6b74b075af3acf992cd2a10a1a08cf2104dfb5f1
Donia Shaban <doniashaban723@gmail.com>
2026-09-19 18:10:10 +0300

    feat(lookups): add ListGovernorates cached query and handler (FR-18)

---
+++ Identity.Application/Features/GeographicLookup/Queries/ListGovernorates/ListGovernoratesQuery.cs
@@ -0,0 +1,14 @@
+﻿using Identity.Application.Common.Interfaces;
+using Identity.Application.Features.GeographicLookup.Dtos;
+using Identity.Domain.Common.Results;
+using MediatR;
+
+namespace Identity.Application.Features.GeographicLookup.Queries.ListGovernorates;
+
+public sealed record ListGovernoratesQuery : ICachedQuery<Result<List<GovernorateDto>>>
+{
+    public string CacheKey => "lookups:governorates";
+    public string[] Tags => ["governorates"];
+    public TimeSpan Expiration => TimeSpan.FromHours(24);
+    public TimeSpan? LocalCacheExpiration { get; init; } = TimeSpan.FromMinutes(5);
+}
