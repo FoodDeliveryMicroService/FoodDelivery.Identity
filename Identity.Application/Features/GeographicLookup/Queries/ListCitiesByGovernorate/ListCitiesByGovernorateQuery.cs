341967ca2023b9fa3849dfe27b516cbe289b7ef6
Donia Shaban <doniashaban723@gmail.com>
2026-09-19 18:10:11 +0300

    feat(lookups): add ListCitiesByGovernorate cached query, handler and validator (FR-19)

---
+++ Identity.Application/Features/GeographicLookup/Queries/ListCitiesByGovernorate/ListCitiesByGovernorateQuery.cs
@@ -0,0 +1,14 @@
+﻿using Identity.Application.Common.Interfaces;
+using Identity.Application.Features.GeographicLookup.Dtos;
+using Identity.Domain.Common.Results;
+
+namespace Identity.Application.Features.GeographicLookup.Queries.ListCitiesByGovernorate;
+
+public sealed record ListCitiesByGovernorateQuery(Guid GovernorateId)
+    : ICachedQuery<Result<List<CityDto>>>
+{
+    public string CacheKey => $"lookups:cities:{GovernorateId}";
+    public string[] Tags => ["cities", $"governorate:{GovernorateId}"];
+    public TimeSpan Expiration => TimeSpan.FromHours(24);
+    public TimeSpan? LocalCacheExpiration { get; init; } = TimeSpan.FromMinutes(5);
+}
---
+++ Identity.Application/Features/GeographicLookup/Queries/ListCitiesByGovernorate/ListCitiesByGovernorateQueryValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Identity.Application.Features.GeographicLookup.Queries.ListCitiesByGovernorate;
+
+public sealed class ListCitiesByGovernorateQueryValidator : AbstractValidator<ListCitiesByGovernorateQuery>
+{
+    public ListCitiesByGovernorateQueryValidator()
+    {
+        RuleFor(x => x.GovernorateId).NotEmpty();
+    }
+}
